85701bc8f50c4377c61b8113b7addf01258a52f2
truong <truongphatroy@gmail.com>
2023-04-26 07:32:14 +0900

    finished footer

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,70 +1,85 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import classes from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const HandleNavigate = (event) => {
+    switch (event.target.innerHTML) {
+      case "Home":
+        return navigate("/");
+      case "Shop":
+        return navigate("shop");
+      case "BOUTIQUE":
+        return navigate("/");
+      case "Cart":
+        return navigate("cart");
+      case "Login":
+        return navigate("login");
+    }
+  };
   return (
     // <nav className='navbar navbar-expand-lg navbar-light bg-light'>
     <div className={classes.Navbar}>
       <ul className={classes.list}>
         <li className={classes.listItem}>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-            to='/'
+          <button
+            type='button'
+            onClick={HandleNavigate}
+            className={location.pathname === "/" ? `${classes.active}` : ""}
           >
             Home
-          </NavLink>
+          </button>
         </li>
         <li className={classes.listItem}>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-            to='shop'
+          <button
+            type='button'
+            onClick={HandleNavigate}
+            className={location.pathname === "/shop" ? `${classes.active}` : ""}
           >
             Shop
-          </NavLink>
+          </button>
         </li>
       </ul>
       <ul className={classes.list}>
         <li className={classes.listItem}>
-          <NavLink to='/'>BOUTIQUE</NavLink>
+          <button type='button' onClick={HandleNavigate}>
+            BOUTIQUE
+          </button>
         </li>
       </ul>
       <ul className={classes.list}>
         <li className={classes.listItem}>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 640 512'>
-            <path d='M32 0C14.3 0 0 14.3 0 32S14.3 64 32 64H48c8.8 0 16 7.2 16 16V368c0 44.2 35.8 80 80 80h18.7c-1.8 5-2.7 10.4-2.7 16c0 26.5 21.5 48 48 48s48-21.5 48-48c0-5.6-1-11-2.7-16H450.7c-1.8 5-2.7 10.4-2.7 16c0 26.5 21.5 48 48 48s48-21.5 48-48c0-5.6-1-11-2.7-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32H144c-8.8 0-16-7.2-16-16V80C128 35.8 92.2 0 48 0H32zM192 80V272c0 26.5 21.5 48 48 48H560c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H464V176c0 5.9-3.2 11.3-8.5 14.1s-11.5 2.5-16.4-.8L400 163.2l-39.1 26.1c-4.9 3.3-11.2 3.6-16.4 .8s-8.5-8.2-8.5-14.1V32H240c-26.5 0-48 21.5-48 48z' />
-          </svg>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-            to='cart'
+          <button
+            type='button'
+            onClick={HandleNavigate}
+            className={location.pathname === "/cart" ? `${classes.active}` : ""}
           >
-            Cart
-          </NavLink>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 640 512'>
+              <path d='M32 0C14.3 0 0 14.3 0 32S14.3 64 32 64H48c8.8 0 16 7.2 16 16V368c0 44.2 35.8 80 80 80h18.7c-1.8 5-2.7 10.4-2.7 16c0 26.5 21.5 48 48 48s48-21.5 48-48c0-5.6-1-11-2.7-16H450.7c-1.8 5-2.7 10.4-2.7 16c0 26.5 21.5 48 48 48s48-21.5 48-48c0-5.6-1-11-2.7-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32H144c-8.8 0-16-7.2-16-16V80C128 35.8 92.2 0 48 0H32zM192 80V272c0 26.5 21.5 48 48 48H560c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H464V176c0 5.9-3.2 11.3-8.5 14.1s-11.5 2.5-16.4-.8L400 163.2l-39.1 26.1c-4.9 3.3-11.2 3.6-16.4 .8s-8.5-8.2-8.5-14.1V32H240c-26.5 0-48 21.5-48 48z' />
+            </svg>
+            <span>Cart</span>
+          </button>
         </li>
         <li className={classes.listItem}>
-          <svg
-            className={classes.svgLogin}
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 448 512'
-          >
-            <path d='M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z' />
-          </svg>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
+          <button
+            type='button'
+            onClick={HandleNavigate}
+            className={
+              location.pathname === "/login" ? `${classes.active}` : ""
             }
-            to='login'
           >
-            Login
-          </NavLink>
+            <svg
+              className={classes.svgLogin}
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 448 512'
+            >
+              <path d='M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z' />
+            </svg>
+            <span>Login</span>
+          </button>
         </li>
       </ul>
     </div>
--- src/page/RootLayout.js
+++ src/page/RootLayout.js
@@ -3,13 +3,16 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
+import { Wrapper } from "../component/Wrapper";
 
 const RootLayout = () => {
   return (
     <>
       <div>
-        <Navbar />
-        <Outlet />
+        <Wrapper>
+          <Navbar />
+          <Outlet />
+        </Wrapper>
         <Footer />
       </div>
     </>
