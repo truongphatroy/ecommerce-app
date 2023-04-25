9aa4c9e040f6562dcb91e54d45596e31a365dfc8
truong <truongphatroy@gmail.com>
2023-04-25 18:09:07 +0900

    setting router OK

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { BrowserRouter, routes, route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./page/HomePage";
 import ShopPage from "./page/ShopPage";
+import DetailPage from "./page/DetailPage";
+import CartPage from "./page/CartPage";
+import CheckoutPage from "./page/CheckoutPage";
+import LoginPage from "./page/LoginPage";
+import RegisterPage from "./page/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <routes>
-        <route path='/' element={<HomePage />} />
-        <route path='/detail' element={<ShopPage />} />
-      </routes>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/detail/:productId' element={<DetailPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
